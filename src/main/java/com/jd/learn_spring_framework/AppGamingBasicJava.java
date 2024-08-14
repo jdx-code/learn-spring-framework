6db1d6513d9183e1bddae9c3f0cb73eec3366643
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-08-14 13:12:59 +0530

    Tightly coupled examples

--- src/main/java/com/jd/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/jd/learn_spring_framework/AppGamingBasicJava.java
@@ -6,12 +6,10 @@ import com.jd.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main (String[] args) {
-
         // var marioGame = new MarioGame();
         // var gameRunner = new GameRunner(marioGame);
         var superContraGame = new SuperContraGame();
         var gameRunner = new GameRunner(superContraGame);
         gameRunner.run();
-
     }
 }
