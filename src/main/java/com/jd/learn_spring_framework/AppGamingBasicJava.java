8272eefb9fb34dfcae4e269e0ce8e957d8ccc658
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-08-14 13:43:49 +0530

    Loosely coupled with Java Interface

--- src/main/java/com/jd/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/jd/learn_spring_framework/AppGamingBasicJava.java
@@ -1,15 +1,16 @@
 package com.jd.learn_spring_framework;
 
 import com.jd.learn_spring_framework.game.GameRunner;
-// import com.jd.learn_spring_framework.game.MarioGame;
+import com.jd.learn_spring_framework.game.MarioGame;
+import com.jd.learn_spring_framework.game.PacmanGame;
 import com.jd.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main (String[] args) {
-        // var marioGame = new MarioGame();
-        // var gameRunner = new GameRunner(marioGame);
-        var superContraGame = new SuperContraGame();
-        var gameRunner = new GameRunner(superContraGame);
+        // var game = new MarioGame();        
+        // var game = new SuperContraGame();
+        var game = new PacmanGame();
+        var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
 }
