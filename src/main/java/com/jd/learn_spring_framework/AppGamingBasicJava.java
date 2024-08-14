5016078c9728f428924a5ca0d92647ec6ca7c303
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-08-14 14:21:40 +0530

    Loosely coupled with Java Interface

--- src/main/java/com/jd/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/jd/learn_spring_framework/AppGamingBasicJava.java
@@ -9,8 +9,8 @@ public class AppGamingBasicJava {
     public static void main (String[] args) {
         // var game = new MarioGame();        
         // var game = new SuperContraGame();
-        var game = new PacmanGame();
-        var gameRunner = new GameRunner(game);
+        var game = new PacmanGame(); // 1. new Object created for Pacman class
+        var gameRunner = new GameRunner(game);  // new Object created for GameRunner class + Wiring od dependencies (game is taken as a dependency of Gamerunner)
         gameRunner.run();
     }
 }
